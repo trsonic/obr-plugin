4b92377a7ee1ad59d3e6c96c2d5704179b2962a4
Tomasz Rudzki <tomasz.rudzki@york.ac.uk>
2024-09-17 17:03:55 +0200

    iamfbr wrapper update, cmake update, submodule update

--- source/iamfbr_wrapper.h
+++ source/iamfbr_wrapper.h
@@ -2,9 +2,8 @@
 // Created by Tomasz Rudzki on 02/07/2024.
 //
 
-#include "ambisonic_encoder.h"
+#include "../iamfbr/src/iamfbr_impl.h"
 #include "extras.h"
-#include "iamfbr_impl.h"
 
 #ifndef IAMFBR_IAMFBR_WRAPPER_H
 #define IAMFBR_IAMFBR_WRAPPER_H
@@ -20,7 +19,6 @@ class iamfbrWrapper {
     iamfbr::InputType input_type;
     switch (plugin_state.input_type) {
       case InputType::Ambisonics:
-        encoder_.reset();
         switch (plugin_state.ambisonic_order) {
           case 3:
             input_type = iamfbr::InputType::k3OA;
@@ -31,40 +29,31 @@ class iamfbrWrapper {
         }
         break;
       case InputType::Loudspeaker_feeds:
-        encoder_.reset();
 
         switch (plugin_state.selected_preset_id) {
           case 1:
             input_type = iamfbr::InputType::k714;
             break;
-          case 2:
-            input_type = iamfbr::InputType::k51;
-            break;
+            //          case 2:
+            //            input_type = iamfbr::InputType::k51;
+            //            break;
         }
         break;
       case InputType::Individual_sources:
-        encoder_ = std::make_unique<iamfbr::ambisonic_encoder>(
-            buffer_size_per_channel, 64, plugin_state.ambisonic_order);
-
-        input_type = iamfbr::InputType::k7OA;
-        break;
+        DBG("Individual sources not supported yet.");
+        return;
     }
 
-    iamfbr_ = std::make_unique<iamfbr::iamfbr_impl>(buffer_size_per_channel,
-                                                    sampling_rate, input_type);
+    iamfbr_ = std::make_unique<iamfbr::IamfbrImpl>(buffer_size_per_channel,
+                                                   sampling_rate, input_type);
   }
 
   void update_source(int input_channel, float gain, float azimuth,
                      float elevation, float distance) {
-    if (encoder_ == nullptr) return;
-    encoder_->set_source(input_channel, gain, azimuth, elevation, distance,
-                         iamfbr::SHCalculationMethod::Generate);
+    // Setup iamfbr.
   }
 
-  void deinitialize() {
-    encoder_.reset();
-    iamfbr_.reset();
-  }
+  void deinitialize() { iamfbr_.reset(); }
 
   void process(juce::AudioBuffer<float>& buffer) {
     if (!iamfbr_) return;
@@ -86,20 +75,7 @@ class iamfbrWrapper {
                 input_buffer.begin() + channel * numSamples);
     }
 
-    if (encoder_ != nullptr) {
-      // buffer to store encoded Ambisonic scene
-      // get number of output channels from the encoder
-      size_t num_output_channels = encoder_->get_number_of_output_channels();
-
-      std::vector<float> intermediate_buffer(
-          static_cast<size_t>(numSamples * num_output_channels), 0.0f);
-
-      encoder_->process_planar_audio_data(input_buffer, intermediate_buffer);
-      iamfbr_->process_planar_audio_data(intermediate_buffer, output_buffer);
-
-    } else {
-      iamfbr_->process_planar_audio_data(input_buffer, output_buffer);
-    }
+    iamfbr_->process_planar_audio_data(input_buffer, output_buffer);
 
     // copy data from output_buffer to buffer
     for (int channel = 0; channel < numChannels; ++channel) {
@@ -114,6 +90,5 @@ class iamfbrWrapper {
   }
 
  private:
-  std::unique_ptr<iamfbr::ambisonic_encoder> encoder_;
-  std::unique_ptr<iamfbr::iamfbr_impl> iamfbr_;
+  std::unique_ptr<iamfbr::IamfbrImpl> iamfbr_;
 };
